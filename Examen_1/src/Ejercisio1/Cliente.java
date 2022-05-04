7a868d157f92379e69d9b449ca8a1fb116c0f989
Alvaro <alvaroalva@upeu.edu.pe>
2022-05-04 15:55:23 -0500

    V2

--- Examen_1/src/Ejercisio1/Cliente.java
+++ Examen_1/src/Ejercisio1/Cliente.java
@@ -3,14 +3,13 @@ package Ejercisio1;
 import java.util.Random;
 
 public class Cliente implements Runnable{
-//6.Se crean las variables de la clase puerta, almacen, nombre.Estas seran recibidas atrabas del constructor. Una varia final de los intentos que puede
-	//hacer los clientes y una variable random que genera un numero aleatorio
+
         Puerta      puerta;
         Almacen     almacen;
         String      nombre;
         Random      generador;
         final int MAX_INTENTOS  =   10;
-//7.Se reciben los objetos de puerta, almacen y el nombre que se mandaron en el lanzador, por ultimo se creara un nuevo generador
+
         public Cliente(Puerta p, Almacen a, String nombre){
                 this.puerta     =   p;
                 this.almacen    =   a;
@@ -18,7 +17,7 @@ public class Cliente implements Runnable{
                 generador       =   new Random();
         }
 
-        //8.Se crea un metodo void para poder generar el tiempo de espera que se requiera
+        
         public void esperar(){
                 try {
                         int ms_azar = generador.nextInt(100);
@@ -29,23 +28,19 @@ public class Cliente implements Runnable{
         }
         @Override
         public void run() {
-        	//9.Iniciando el hilo se creara un for que se recorrera 10 veces, el numero maximo de intentos
+        	
                 for (int i=0; i<MAX_INTENTOS; i++){
              
-                	//10.Tenemos un if que validara que el metodo de puerta creada y si este es false, lo cual pasa 
-                	//la primera vez que se recorre
+                
                         if (!puerta.estaOcupada()){
-                    //13.Se habre otro if que llamara al metodo intentar entrar que retorna un valor de 
-                    //true o falso
+                  
                                 if (puerta.intentarEntrar()){
-                                	//15.Cuando se pase el if, es porque el valor ha regrasado como true
-                                	//en este if se esperar un tiempo ya establecido y por ultimo se cerrara la puerta
+                              
                                         esperar();
                                         puerta.liberarPuerta();
-                                        //16.Al entrar a la puerta se llamara al al metodo de almacen que devuelve un booleano true o false
+                                        
                                         if (almacen.cogerProducto()){
-                                        	//18. Si se consigue un true entonces se retornara el exito en caso de cuzar
-                                        	//se devolvera que no se pudo adquirir un producto
+                                        
                                                 System.out.println(
                                                                 this.nombre + ": cogí un producto!");
                                                 return ;
@@ -62,9 +57,7 @@ public class Cliente implements Runnable{
                         }
 
                 } 
-                //19.Al recorrese y no haber entrado se imprimira un mensaje de que no se tuvo exito
-                //Fin del for
-                //Se superó el máximo de reintentos y abandonamos
+              
                 System.out.println(this.nombre+
                                 ": lo intenté muchas veces y no pude");
         }
--- Examen_1/src/Ejercisio1/GrandesAlmacenes.java
+++ Examen_1/src/Ejercisio1/GrandesAlmacenes.java
@@ -3,33 +3,31 @@ package Ejercisio1;
 public class GrandesAlmacenes {
     public static void main(String[] args) throws InterruptedException {
             final int NUM_CLIENTES  = 10;
-            //1. Primero definimos como variables finales el numero de cliente y el numero total de productos que va haber en un almacen
+           
             final int NUM_PRODUCTOS = 5;
          
-            //2.Se crean un array de la clase clientes que seran los distitnos clientes que habran, una unica clase de almacen para el
-            //numero total de productos y la puerta que sera la que permitira entrar a los cliente, por ultimo se crea un array de thread para poder
-            //lanzar los distintos clientes que se crearan
+           
             Cliente[]   cliente =   new Cliente[NUM_CLIENTES];
             Almacen     almacen =   new Almacen(NUM_PRODUCTOS);
             Puerta      puerta  =   new Puerta();
 
             Thread[]    hilosAsociados=new Thread[NUM_CLIENTES];
 
-            //3.Se recorre un for donde se crara un cliente nueve hasta la cantidad de clientes que pusimos que pusimos al principio
+       
             for (int i=0; i<NUM_CLIENTES; i++){
-            	    //4.Con cada cliente que se cree se le asignara un nombre y se lo enviara a un constructor Cliente con las clases de puerta y almacen
+            	    
                     String nombreHilo   = "Cliente "+i;
                     cliente[i]          = new Cliente(puerta, almacen, nombreHilo);
                                                                                            
                     hilosAsociados[i]   = new Thread(cliente[i]);
-                    //Intentamos arrancar el hilo
+                    
                     hilosAsociados[i].start();
-            } //Fin del for
+            } 
 
-            //5.Una vez que se arraquen los clientes entonces se le dara un join para que estos se ejcuten primero.
+           
             for (int i=0; i<NUM_CLIENTES; i++){
                     hilosAsociados[i].join();
-            } //Fin del for
+            } 
            
     
 }
